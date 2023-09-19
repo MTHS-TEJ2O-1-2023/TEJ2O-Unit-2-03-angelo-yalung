47cdba10ed41c32974196dcd4fe9c18d7e2d4d05
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-19 19:36:32 -0400

    Update main.ts, README.md

--- main.ts
+++ main.ts
@@ -8,9 +8,11 @@
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('Area = 3 x 5' + (3 * 5).toString())
+basic.showString('The area for a Rectangle that is 3x5 =' + (3 * 5).toString())
+basic.showString('cm')
 
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString(('Perimeter = 2 x (5+3)' + (2*(3 + 5)).toString()))
+basic.showString(('The perimiter for a Rectangle that is = 2 x (5+3)' + (2*(3 + 5)).toString()))
+basic.showString('cm') 
