160e1f87afd70a8bdfe57808ea577e52a3774f6b
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-19 19:42:22 -0400

    microbit

--- main.ts
+++ main.ts
@@ -2,10 +2,17 @@
  *
  * Created by: Angelo Yalung
  * Created on: Sep 2023
- * This program does math.
+ * This program does basic math.
 */
 
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString((5 + 5 ** 5).toString())
+basic.showString('The area for a Rectangle that is 3x5 =' + (3 * 5).toString())
+basic.showString('cm')
+
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString(('The perimiter for a Rectangle that is = 2 x (5+3)' + (2 * (3 + 5)).toString()))
+basic.showString('cm')
