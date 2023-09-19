af9f0cdb7558c0115df552564b9aabb069dbbfa4
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-19 19:41:57 -0400

    Restore '9/19/2023, 1:08:34 PM microbit does math (not clickbait)'

--- main.ts
+++ main.ts
@@ -8,11 +8,4 @@
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('The area for a Rectangle that is 3x5 =' + (3 * 5).toString())
-basic.showString('cm')
-
-basic.clearScreen()
-basic.pause(1000)
-
-basic.showString(('The perimiter for a Rectangle that is = 2 x (5+3)' + (2*(3 + 5)).toString()))
-basic.showString('cm') 
+basic.showString((5 + 5 ** 5).toString())
